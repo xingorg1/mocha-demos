070d6d374d8d00d3600cf8645ddfac0e36e320c3
xingorg1 <xing.org1@outlook.com>
2021-03-11 21:29:50 +0800

    mocha test

--- demo01/add.test.js
+++ demo01/add.test.js
@@ -6,3 +6,9 @@ describe('加法函数的测试', function() {
     expect(add(1, 1)).to.be.equal(2);
   });
 });
+
+describe('加法函数的测试2', function() {
+  it('2 加 3 应该 不等于 4', function() {
+    expect(add(2, 3)).to.be.not.equal(4);
+  });
+});
