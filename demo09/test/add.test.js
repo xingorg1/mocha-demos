4e7be9ea4db340824165f5a053d28cc2b7250d85
xingorg1 <xing.org1@outlook.com>
2021-03-12 11:19:23 +0800

    mocha report types

--- demo09/test/add.test.js
+++ demo09/test/add.test.js
@@ -3,7 +3,7 @@ var expect = require('chai').expect;
 
 describe('加法函数的测试', function() {
   it('1 加 1 应该等于 2', function() {
-    expect(add(1, 1)).to.be.equal(4);
+    expect(add(1, 1)).to.be.equal(2);
   });
 
   it('任何数加0应该等于自身', function() {
